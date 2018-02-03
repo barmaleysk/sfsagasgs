536404b324eb39adaf18e5d2fc7bdb9b06ebe03d
Nity108 <nitayl@list.ru>
2018-02-04 01:17:53 +0300

    mlab

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     //TOKEN: '451169951:AAEsAl5dhPOqomw4bndqtPmVywBrjg1QK8o' // nitytestbot
     TOKEN: '482824206:AAGLqBHvpHvXXu5_gsY1eLBjlDayXkdLJEM', // FarmerGameBot
-    DB_URL: 'mongodb://localhost/farmer-game'
+//    DB_URL: 'mongodb://localhost/farmer-game'
+//    local
+    DB_URL: 'mongodb://Nity:627886nar@ds046677.mlab.com:46677/farmergame'
+//    mongodb mLab
 }
