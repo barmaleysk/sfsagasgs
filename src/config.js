9d558d6c4fd595c71c8b25d07eac99076769648f
Nity108 <nitayl@list.ru>
2018-02-15 13:58:19 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //TOKEN: '451169951:AAEsAl5dhPOqomw4bndqtPmVywBrjg1QK8o' // nitytestbot
     TOKEN: '482824206:AAGLqBHvpHvXXu5_gsY1eLBjlDayXkdLJEM', // FarmerGameBot
-    DB_URL: 'mongodb://localhost/farmer-game' // local
-//    DB_URL: 'mongodb://Nity:627886nar@ds046677.mlab.com:46677/farmergame' // mLab
+//    DB_URL: 'mongodb://localhost/farmer-game' // local
+    DB_URL: 'mongodb://Nity:627886nar@ds046677.mlab.com:46677/farmergame' // mLab
 }
