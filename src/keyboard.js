a9f17052f77fd06cc4c7334eb42661025ed61f39
Nity108 <nitayl@list.ru>
2018-02-15 12:21:35 +0300

    Update keyboard.js

--- src/keyboard.js
+++ src/keyboard.js
@@ -56,7 +56,7 @@ module.exports = {
 //        [kb.casino.bowling, kb.casino.lottery],
 //        [kb.casino.dice, kb.casino.darts],
 //        [kb.casino.slot_machine, kb.casino.guess_suit],
-        [kb.casino.bonus, kb.back_city]
+//        [kb.casino.bonus, kb.back_city]
     ],
     friends: [
         
