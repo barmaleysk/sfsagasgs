30bd4a802b2d7cb0ab625940e5d779d7512bcb7d
Nity108 <nitayl@list.ru>
2018-02-15 12:08:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ helper.logStart()
 setInterval(function(){
     var hm = new Date().getHours() + ':' + new Date().getMinutes()
     
-    console.log(lang, kb.home.myFarm)
+    console.log(lang, ikb.bank[1][0])
     
     switch (hm) {
         case '0:30':
