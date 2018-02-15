530df6c53292f7fee584aefece45bccbe7fe4bbf
Nity108 <nitayl@list.ru>
2018-02-15 08:01:55 +0300

    Add files via upload

---
+++ keyboard-buttons.ru.en.js
@@ -0,0 +1,101 @@
+module.exports = {
+    home: {
+        myFarm: '🏡 My farm'
+        friends: '🏘 Friends'
+        city: '🏢 City'
+        other: '✨ Miscellaneous'
+    }
+    city: {
+        townHall: '⛪️ Town Hall'
+        market: '🛒 Market'
+        bank: '🏦 Bank'
+        casino: '🎰 Casino'
+    }
+    other: {
+        tasks: '📋 Jobs'
+        settings: '⚙️ Settings'
+        help: '❓ Help'
+        community: '💬 Communities'
+    }
+    townHall: {
+        statistics: '📊 Statistics'
+        name: '📝 farmer License'
+    }
+    myFarm: {
+        plants: '🌱 Plants'
+        buildings: '🏫 Buildings'
+        warehouse: '📦 Warehouse'
+        statistics: '📊 Statistics'
+        referals: '👥 Referrals'
+        garage: '🚗 Transport'
+    }
+    plants: {
+        fruit: '🍎 Fruits'
+        vegetables: '🌽 Vegetables'
+        buy: '🌱 Buy' 
+    }
+    fruit: {
+        buy: 'Buy 🍎 Fruits'
+        send: '📦 Send 🍎 warehouse'
+    }
+    vegetables: {
+        buy: 'Buy 🌽 Vegetables'
+        send: '📦 Send 🌽 warehouse'
+    }
+    buildings: {
+        build: '🏫 build new'
+        send: '📦 Send 🥚 warehouse'
+        buildOne: '🏫 Build'
+    }
+    friends: {
+        
+    }
+    market: {
+        plants: 'Sell 🌱 Plants'
+        products: 'Sell Products 🥚'
+    }
+    warehouse: {
+        fruit: 'More about 🍎 Fruits'
+        vegetables: 'More about 🌽 Vegetables'
+        products: 'More about 🥚 Products'
+    }
+    settings: {
+        notifications: '🔔 alert'
+    }
+    bank: {
+        deposit: '💸 Fill up balance'
+        exchange: '💱 Currency Exchange'
+        withdraw: '📤 Display'
+        redeem: '💼 Redeem plot'
+    }
+    help: {
+        
+    }
+    casino: {
+        dice: '🎲 ​​Bones'
+        bonus: '🎁 Daily bonus'
+        darts: '🎯 Darts'
+        slot_machine: '🎰 Slot Machines'
+        lottery: '🎟 Lottery'
+        guess_suit: '🃏 Guess the suit'
+        bowling: '🎳 Bowling'
+    }
+    languages: {
+        english: '🇬🇧 English',
+        russian: '🇷🇺 Russian'
+        german: '🇩🇪 Deutsch 🤖',
+        french: '🇫🇷 Français 🤖',
+        portuguese: '🇵🇹 Portugues 🤖',
+        spanish: '🇪🇸 Español 🤖'
+    }
+    back: '⬅️ Back'
+    back_farm: '🏡 Back'
+    back_city: '🏢 Back'
+    skip: '📰',
+    It continues: '🗞',
+    step: '📃',
+    changeName: '📝 Change the name of'
+    cancel: '✖️ Cancel'
+    yes: '✔️ Yes'
+    no: '❌ No'
+}
