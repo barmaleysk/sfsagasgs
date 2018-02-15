a4727010a09078113dcd91f07a40271dfce09771
Nity108 <nitayl@list.ru>
2018-02-15 13:37:38 +0300

    Create texts-en.js

---
+++ src/texts-en.js
@@ -0,0 +1,51 @@
+`Welcome to FarmerGame! In this game You can unleash Your potential farmer growing vegetables or fruits or to get the product from the animals and selling them!\n\nLet's will not waste time and immediately move on to learning! Click 🗞, to go to training or 📰, to skip his`,
+    
+    `<b> Let's start!</b> 
+From a small garden to the huge farm You will find a lot of interesting things. But I will not rush You.
+ 
+At this point in the game there are two kinds of plant and animal products:
+1. 🍎 Fruits.
+2. 🌽 Vegetables.
+3. 🥚 products.
+ 
+<B> Push 📃, to find out more:) </ b> `,
+    
+    `<B> Step 2 </ b>
+🍎 Fruits and Vegetables 🌽 grow and sell for dollars and 💵 💰 Gold. 
+🥚 products are extracted with 🏫 Buildings with animals, and they can be sold for Euro 💶 and 💎 Diamond. 
+For 💵 dollars you can buy 🌱 plants for planting, and for 💶 Euro - build 🏫 Buildings for animals.
+Gold and Diamond can be exchanged for real money.
+<Code> To date, the exchange rate is as follows: 
+100 Gold = $ 1.
+100 Diamond = 0.0002 BTC. </ Code>
+If you're ready to move on PUSH 📃, it left quite a bit! `
+    
+    `All the plants and products have on 6️⃣ species. The more expensive kind, the more will grow and produce products.
+  
+For example, ⛽️ Petrochemical pump 100 is 1 level and produces 🌑 OilCoin 🛢 16 barrels of oil per hour, whereas Petrochemical pump 6 OilCoin level is 100,000 and produces 31,250 barrels per hour. 
+You feel the difference? Yes, she's just ogramnaya! Just for those who want to develop quickly, we have created 🏦 Bank. There you can buy OilCoin and another currency for the purchase of plants, using real money. Do not forget that all the investment required to return to the form of currency that can be displayed as real money :)
+Push Okay, to find out more! `
+    
+    `Deposit - not ednistvenny fastest way to get in-game currency. We also implemented a smart referral system!
+  
+For every friend you will receive a lump sum of 40 dollars and 20 💵 💶 Euro just like that! Also, you will get a% of the completion of its accounts and% of their harvests. But more about that a little later.
+Push 📃, to complete the theory and begin to practice! `
+    
+    'I see you are tired of reading so much text. Let's get down to the game itself! At the bottom you see a menu with buttons. Click on the button 📋 tasks to learn the current job and move to implement it! `
+    
+    `🏡 <b> My Farm </ b> \ n \ nHere you will find basic information on your ferme.`,
+    
+    `📃 Main menu`,
+    
+    `🏢 City \ n \ nHere You can visit different zavedeniya.`,
+    
+    `🎰 Casino \ n \ nHere You can play different types of games or just get 🎁 Daily bonus.`,
+    
+    `⛪️ Town Hall \ n \ nIn the town hall you can get the farmer a license that allows you to name your farm, change an existing name, see your stats,`
+    
+    `✨ Other \ n \ nHere You can find your settings, tasks, ask for help or visit our chaty.`,
+    
+    `🌱 <b> Plants </ b>
+  
+Here you can buy a variety of plants. On plants grown products, you later can send to the warehouse and to sell on the market for dollars and 💵 💰 Gold, which subsequently can be displayed as real money!
+<I> You give 30% of the resources you extracted to your landlord. </ I> `
