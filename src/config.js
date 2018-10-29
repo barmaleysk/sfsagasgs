3d3be927b5c96bc1748fb8aa042cc41809dfcf48
barmaleysk <43924454+barmaleysk@users.noreply.github.com>
2018-10-29 14:16:17 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //TOKEN: '451169951:AAEsAl5dhPOqomw4bndqtPmVywBrjg1QK8o' // nitytestbot
-    TOKEN: '482824206:AAGLqBHvpHvXXu5_gsY1eLBjlDayXkdLJEM', // FarmerGameBot
+    TOKEN: '699518045:AAElMrJbvXxWCXPj-VvEHLKWiyQ0FMH9ttY', // FarmerGameBot
 //    DB_URL: 'mongodb://localhost/farmer-game' // local
     DB_URL: 'mongodb://Nity:627886nar@ds046677.mlab.com:46677/farmergame' // mLab
 }
