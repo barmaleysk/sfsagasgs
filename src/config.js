6c5f442df0b9a0610f6955e25787b535410ec3a7
barmaleysk <43924454+barmaleysk@users.noreply.github.com>
2018-10-29 15:09:03 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //TOKEN: '451169951:AAEsAl5dhPOqomw4bndqtPmVywBrjg1QK8o' // nitytestbot
     TOKEN: '699518045:AAElMrJbvXxWCXPj-VvEHLKWiyQ0FMH9ttY', // FarmerGameBot
-//    DB_URL: 'mongodb://localhost/farmer-game' // local
-    DB_URL: 'mongodb://Nity:627886nar@ds046677.mlab.com:46677/farmergame' // mLab
+//    DB_URL: 'mongodb://localhost/farmer-game' // local 
+    DB_URL: 'mongodb://user:TIdHopCaHybUpy6@ds143893.mlab.com:43893/test14214' // mLab
 }
